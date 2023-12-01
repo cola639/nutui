bb00ebe2f08a9dbf84494d43d92fc81499e1cf37
cola639 <337507950@qq.com>
2023-12-01 11:25:28 +0800

    chore: 更新GPT目录分析

--- src/packages/utils/create/component.ts
+++ src/packages/utils/create/component.ts
@@ -1,4 +1,6 @@
 import { App, defineComponent, ComponentOptions } from 'vue';
+
+// 转大写
 const camelize = (s: string) => s.replace(/-./g, (x) => x[1].toUpperCase());
 
 export function createComponent(name: string) {
